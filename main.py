f5338c53f7255d538e52adc5cb5bc85b733d0d53
techwithguga <guramxatiashvili@hotmail.com>
2024-02-01 12:37:54 +0000

    add ascii art, other characters in text, restart function, shift more than 26

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
 def caesar(start_text, shift_amount, cipher_direction):
   end_text = ""
   if cipher_direction == "decode":
-      shift_amount *= -1
-  for letter in start_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    end_text += alphabet[new_position]
-  print(f"Here's the {direction}d result: {end_text}")
+    shift_amount *= -1
+  for char in start_text:
+    #if user enters space or any other character, just add it to the end_text
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+#print logo
+from art import logo
+print(logo)
+
+#restart the program
+should_end = False
+while not should_end:
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  #if user enters shift more than 26
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
+
+
+
